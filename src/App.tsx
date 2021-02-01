7cb94f7b377f5abbb54e50f4f5885c69ea7e738b
Adam Antkowiak <adam.antkowiak93@gmail.com>
2021-02-01 17:53:35 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TestComponent from "./TestComponent";
+import "./App.css";
 
-function App() {
+import { useClickOutside } from "./useClickOutside";
+
+const App = () => {
+  const { onStartListeningClickOutside } = useClickOutside();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TestComponent clicked={onStartListeningClickOutside} />
+      <div
+        style={{ minHeight: "100vh", background: "#347643" }}
+        className="frontPage"
+      ></div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/TestComponent.tsx
@@ -0,0 +1,5 @@
+const TestComponent = ({ clicked }) => {
+  return <button onClick={clicked}>BLEB</button>;
+};
+
+export default TestComponent;
